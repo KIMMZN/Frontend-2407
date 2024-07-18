4348731b553aba45423a8701e9d3dde5c6de18f5
[sion <tkdlqhrm222@gmail.com>
2024-07-18 17:30:49 +0900

    7.18 thusday. last

--- JS/ch03.조건문/Q2.사분면2.js
+++ JS/ch03.조건문/Q2.사분면2.js
@@ -4,10 +4,12 @@ let y = Math.ceil(Math.random() * 20);
 
 x *= Math.random() > 0.5 ? 1 : -1;
 y *= Math.random() > 0.5 ? 1 : -1;
-
+/* 
 console.log(x, y);
 
 Quadrant = "";
+ */
+
 
 if (x > 0 && y > 0) Quadrant = "Quadrant 1";
 else if (x > 0 && y < 0) Quadrant = "Quadrant 4";
@@ -16,3 +18,4 @@ else if (x < 0 && y < 0) Quadrant = "Quadrant 3";
 else Quadrant = "좌표축";
 
 console.log(x, y, Quadrant);
+ 
--- JS/ch05.함수/12.Array.js
+++ JS/ch05.함수/12.Array.js
@@ -1,4 +1,20 @@
 // 배열에 대한 고급 함수 ;
 
 // forEach
-let fruits = '수박,참외,복숭아,토마토,포도'
+let fruits = '수박,참외,복숭아,토마토,포도'.split(',');
+
+for (let fruit of fruits) // 알아야됨
+    console.log(fruit);
+
+fruits.forEach((fruit, index) => { // 좀어려움; 위에것을 확실히 알자;
+    console.log(`인덱스: ${index}, 아이템: ${fruit}`);
+});
+
+let numbers = [11, 23, 56, 72, 83];
+numbers.map((num) => {
+    console.log(`${num}, ${num * num}`);
+});
+
+// 과일의 글자수가 3인 과일만 선택
+let newFruits = fruits.filter((fruit) => fruit.length == 3);
+console.log(newFruits);
